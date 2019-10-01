61635d831e0240de4d193f84df87281814cf731b
W.Presser <W.Presser@SHL125-14>
2019-10-01 14:39:14 -0700

    Why

--- Testing3/Program.cs
+++ Testing3/Program.cs
@@ -6,7 +6,7 @@ namespace Testing3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!");
         }
     }
 }
